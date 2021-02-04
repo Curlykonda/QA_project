0d8832adff0f2158ab01a9e8dacd7429a9e716e8
HB <bartsch.henning@gmail.com>
2021-02-04 09:53:47 +0100

    Add simple embedding layer and option to determine 'freeze'

--- src/modules/embedding.py
+++ src/modules/embedding.py
@@ -1,7 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
 
 from src.modules.encoder import HighwayEncoder
 
@@ -14,21 +12,44 @@ class BiDAF_Embedding(nn.Module):
     Word-level embeddings are further refined using a 2-layer Highway Encoder
 
     Args:
-        word_vectors (torch.Tensor): Pre-trained word vectors.
+        word_vectors (torch.FloatTensor): matrix of shape n_words x embedding_dim,
+                                            either pre-trained or randomly initialised embeddings
         d_hidden (int): Size of hidden activations.
         p_drop (float): Probability of zero-ing out activations
     """
-    def __init__(self, word_vectors : torch.Tensor, d_hidden, p_drop):
+    def __init__(self, word_vectors: torch.FloatTensor, d_hidden, p_drop, n_layers=2, freeze_we=True):
         super(BiDAF_Embedding, self).__init__()
-        self.drop_prob = p_drop
-        self.embed = nn.Embedding.from_pretrained(word_vectors)
+
+        self.dropout_module = nn.Dropout(p=p_drop)
+
+        if word_vectors is not None:
+            self.embed = nn.Embedding.from_pretrained(word_vectors, freeze=freeze_we)
+        else:
+            raise ValueError("Need to provide embedding matrix")
+
         self.proj = nn.Linear(word_vectors.size(1), d_hidden, bias=False)
-        self.hwy = HighwayEncoder(n_layers=2, d_hidden=d_hidden)
+        self.hwy = HighwayEncoder(n_layers=n_layers, d_hidden=d_hidden)
 
     def forward(self, x):
-        emb = self.embed(x)   # (batch_size, seq_len, embed_size)
-        emb = F.dropout(emb, self.drop_prob, self.training)
+        """ x contain sequences encoded as word indices """
+        emb = self.dropout_module(self.embed(x))   # (batch_size, seq_len, embed_size)
         emb = self.proj(emb)  # (batch_size, seq_len, hidden_size)
         emb = self.hwy(emb)   # (batch_size, seq_len, hidden_size)
 
         return emb
+
+class Simple_Embedding(nn.Module):
+    """ Embedding layer to map word indices to corresponding word embeddings """
+
+    def __init__(self, word_vectors: torch.FloatTensor, p_drop=0., freeze_we=False):
+        super(Simple_Embedding, self).__init__()
+
+        self.dropout = nn.Dropout(p_drop)
+        if word_vectors is not None:
+            self.embed = nn.Embedding.from_pretrained(word_vectors, freeze=freeze_we)
+        else:
+            raise ValueError("Need to provide embedding matrix")
+
+    def forward(self, x):
+        """ x contain sequences encoded as word indices """
+        return self.dropout(self.embed(x))
